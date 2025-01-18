3708618584f255305ec1e05d1652c1e5f8e4125c
krysx7003 <krysx7003@gmail.com>
2025-01-18 22:53:21 +0100

    Shit i guess

--- Lab7/js/Lab7.js
+++ Lab7/js/Lab7.js
@@ -58,8 +58,8 @@ function main() {
       },
     };    
     const buffers = initBuffers(gl);
-    const texture1 = loadTexture(gl, "tekstura1.png");
-    const texture2 = loadTexture(gl, "tekstura2.png");
+    const texture1 = loadTexture(gl, "cube");
+    const texture2 = loadTexture(gl, "tetr");
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     function render(now) {
       rotationX = rotateX ? rotationX + speed * 0.002 : rotationX;
@@ -99,51 +99,4 @@ function loadShader(gl, type, source) {
       return null;
     }  
     return shader;
-}
-function loadTexture(gl, url) {
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  const level = 0;
-  const internalFormat = gl.RGBA;
-  const width = 1;
-  const height = 1;
-  const border = 0;
-  const srcFormat = gl.RGBA;
-  const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]);
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    level,
-    internalFormat,
-    width,
-    height,
-    border,
-    srcFormat,
-    srcType,
-    pixel,
-  );
-  const image = new Image();
-  image.onload = () => {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      level,
-      internalFormat,
-      srcFormat,
-      srcType,
-      image,
-    );
-    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-      gl.generateMipmap(gl.TEXTURE_2D);
-    } else {
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    }
-  };
-  image.src = url;
-  return texture;
-}
-function isPowerOf2(value) {
-  return (value & (value - 1)) === 0;
 }
